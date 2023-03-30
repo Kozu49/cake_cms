22749f0688e4770803a72bde21f321699b2ac621
manoj <manoz.kozu@gmail.com>
2023-03-30 15:24:15 +0545

    dynamic content

--- src/Controller/BlogsController.php
+++ src/Controller/BlogsController.php
@@ -16,6 +16,18 @@ class BlogsController extends AppController
 
     public function home()
     {
+        $this->loadModel('Articles');
+        $articles=$this->Articles->find('all')
+                                    ->limit(3);
+        $this->set('articles',$articles);
+
+
+        // $recentArticles = $this->fetchTable('Articles')->find('all', [
+        //     'limit' => 5,
+        //     'order' => 'Articles.created DESC'
+        // ])
+        // ->all();
+
     }
 
     public function about()
