d5424b8b3db29d034ccd300376cda73d7b2b7f37
manoj <manoz.kozu@gmail.com>
2023-03-30 14:56:52 +0545

    Create link & Change layout for a controller also for All controller

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -17,6 +17,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Cake\Controller\Controller;
+use Cake\Event\EventInterface;
 
 /**
  * Application Controller
@@ -50,4 +51,5 @@ class AppController extends Controller
          */
         //$this->loadComponent('FormProtection');
     }
+
 }
--- src/Controller/BlogsController.php
+++ src/Controller/BlogsController.php
@@ -2,14 +2,28 @@
 
 namespace App\Controller;
 
+use Cake\Event\EventInterface;
 use Cake\View\ViewBuilder;
 
 class BlogsController extends AppController
 {
 
-    public function home()
+    public function beforeFilter(EventInterface $event)
     {
         $this->viewBuilder()->setLayout('blog');
+
+    }
+
+    public function home()
+    {
+    }
+
+    public function about()
+    {
+    }
+
+    public function contact()
+    {
     }
 
 }
